ecde7977a64816ef4dce7db88d9d51646a2ce13a
Oleksandr Holub <oleksandr.h@namecheap.com>
2024-04-07 14:39:21 -0500

    added dependencies injection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -22,19 +22,9 @@ func main() {
 	logger := createLogger(cfg)
 	app := &application{logger: logger}
 
-	mux := http.NewServeMux()
-
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
-
-	mux.HandleFunc("GET /{$}", app.home)
-	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
-	mux.HandleFunc("GET /snippet/create", app.getCreateSnippetForm)
-	mux.HandleFunc("POST /snippet/create", app.createSnippet)
-
 	logger.Info("starting server", slog.String("addr", cfg.addr))
 
-	err := http.ListenAndServe(cfg.addr, mux)
+	err := http.ListenAndServe(cfg.addr, app.routes())
 
 	logger.Error(err.Error())
 	os.Exit(1)
