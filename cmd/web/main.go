908ec90138be6c23011c721fa8cd5377e66f03b3
Oleksandr Holub <oleksandr.h@namecheap.com>
2024-04-08 20:09:57 -0500

    added database connection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log/slog"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -12,14 +15,23 @@ type application struct {
 }
 
 type config struct {
-	addr      string
-	staticDir string
-	logSource bool
+	addr             string
+	staticDir        string
+	logSource        bool
+	connectionString string
 }
 
 func main() {
 	cfg := setConfig()
 	logger := createLogger(cfg)
+	db, dbErr := openDbConnection(cfg)
+	if dbErr != nil {
+		logger.Error(dbErr.Error())
+		os.Exit(1)
+	}
+
+	defer db.Close()
+
 	app := &application{logger: logger}
 
 	logger.Info("starting server", slog.String("addr", cfg.addr))
@@ -34,6 +46,7 @@ func setConfig() config {
 	var cfg config
 	flag.StringVar(&cfg.addr, "addr", ":4000", "HTTP network address")
 	flag.StringVar(&cfg.staticDir, "staticDir", "./ui/static", "Path to static assets")
+	flag.StringVar(&cfg.connectionString, "connectionString", "root:golanguserpassword@/snippetbox?parseTime=true", "MySQL connection string")
 	flag.BoolVar(&cfg.logSource, "logSource", false, "Include source file and line number in log output")
 
 	flag.Parse()
@@ -46,4 +59,18 @@ func createLogger(cfg config) *slog.Logger {
 		Level:     slog.LevelDebug,
 		AddSource: cfg.logSource,
 	}))
+}
+
+func openDbConnection(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("mysql", cfg.connectionString)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+	return db, nil
 }
